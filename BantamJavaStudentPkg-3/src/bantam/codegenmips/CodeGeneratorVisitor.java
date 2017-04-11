5d358d427e8806552293aa4f4b68db6f18e37ef1
Alex Rinker <abrinker@colby.edu>
2017-04-11 13:29:43 -0400

    merge our two visitors into one

--- BantamJavaStudentPkg-3/src/bantam/codegenmips/CodeGeneratorVisitor.java
+++ BantamJavaStudentPkg-3/src/bantam/codegenmips/CodeGeneratorVisitor.java
@@ -12,6 +12,8 @@ package bantam.codegenmips;
 
 import bantam.ast.*;
 import bantam.util.ClassTreeNode;
+import bantam.util.Location;
+import bantam.util.SymbolTable;
 import bantam.visitor.Visitor;
 
 import java.io.PrintStream;
@@ -39,6 +41,15 @@ public class CodeGeneratorVisitor extends Visitor{
     /** Map which connects String constants to their label */
     private Map<String, String> stringLabels;
 
+    /** the current varSymbolTable being used */
+    private SymbolTable currClassVarTable;
+
+    /** the current offset being used for the fields */
+    private int currOffset;
+
+    /** a flag representing whether or not we are generating inits this runthrough */
+    private boolean generatingInits;
+
     /**
      * constructor method
      * @param root the root node of the program
@@ -57,21 +68,85 @@ public class CodeGeneratorVisitor extends Visitor{
         this.root = root;
         this.classNames = classNames;
         this.stringLabels = stringLabels;
+        this.currOffset = 12;
     }
 
+    /**
+     * When visiting the class node, the visitor sets up a new symbol table
+     * It generates the label for the init method and then proceeds to
+     * traverse through the labels. After traversal, it generates the code
+     * to close the init method
+     * @param node
+     * @return
+     */
     @Override
     public Object visit(Class_ node) {
-        return super.visit(node);
+        if(generatingInits) {
+            //Update the current class name
+            this.initializeSymbolTable();
+
+            //Generate the init label
+            this.mipsSupport.genLabel(this.currClass.getName() + "_init");
+
+            //Handle Object's init method since it is the base case
+            if (node.getName() == "Object") {
+                this.mipsSupport.genComment("Object's init method is move $v0 $a0");
+                this.mipsSupport.genMove(
+                        this.mipsSupport.getResultReg(),
+                        this.mipsSupport.getArg0Reg()
+                );
+            } else {
+                this.mipsSupport.genComment("Call the parent's init method");
+                this.mipsSupport.genDirCall(node.getParent() + "_init");
+            }
+            super.visit(node);
+            this.currOffset = 12; //reset the offset
+            this.mipsSupport.genRetn(); //generate a return to close the init
+            return null;
+        }
+        else {
+            return super.visit(node);
+        }
     }
 
+    /**
+     * Visits the fields node and updates the value in memory if necessary. It
+     * stores the offset in the appropriate symbol table for future use
+     * @param node
+     * @return
+     */
     @Override
     public Object visit(Field node) {
-        return super.visit(node);
+        if (generatingInits) {
+            super.visit(node); //This will put the expr of the field in $v0
+            Location location = new Location(this.mipsSupport.getArg0Reg(), this.currOffset);
+            if (node.getInit() != null) {
+                //If there is an expression, we have to store the result to memory
+                this.mipsSupport.genComment("Store the initial value of the field in memory");
+                this.mipsSupport.genStoreWord(
+                        this.mipsSupport.getResultReg(),
+                        this.currOffset,
+                        this.mipsSupport.getArg0Reg()
+                );
+            }
+            //Add the field and location to the symbol table
+            this.currClassVarTable.add(node.getName(), location);
+            //update memory for the next
+            this.currOffset += 4;
+            return null;
+        } else {
+            return super.visit(node);
+        }
     }
 
     @Override
     public Object visit(Method node) {
-        return super.visit(node);
+        if (generatingInits) {
+            //Terminate at the method node as this means all fields are generated
+            return null;
+        } else {
+            return super.visit(node);
+        }
     }
 
     @Override
@@ -383,5 +458,40 @@ public class CodeGeneratorVisitor extends Visitor{
         mipsSupport.genLoadAddr(mipsSupport.getResultReg(),
                                 this.stringLabels.get(node.getConstant()));
         return null;
+    }
+
+    //Recursive Initialize Methods
+    /**
+     * Generates init methods for each class in the program
+     * @return
+     */
+    public void generateInits() {
+        generateInits(this.root);
+    }
+
+    /**
+     * Helper method for generateInits. Generates the init methods for
+     * the parent class and all of its immediate children
+     * @param parent
+     */
+    private void generateInits(ClassTreeNode parent) {
+        //Update the symbol table and symbol table for future use
+        this.currClassVarTable = parent.getVarSymbolTable();
+        parent.getASTNode().accept(this);
+
+        //Generate inits for each of the child classes
+        parent.getChildrenList().forEachRemaining( child ->
+                generateInits(child)
+        );
+    }
+
+    //Helper methods
+    /**
+     * Sets up a symbol table for the current class node
+     */
+    private void initializeSymbolTable() {
+        //Set up the class' symbol table
+        this.currClassVarTable = new SymbolTable();
+        this.currClassVarTable.enterScope();
     }
 }
